f589fb93344b6f3eef37be3c7e8716a6a666fca0
jsmartcano <jsmartcano@gmail.com>
2015-10-30 19:53:01 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+//double &f();
+
+//double valor=10.0;
+
+int main() {
+	int i=12;
+	int *px;
+
+	px=&i;
+
+	cout << *px;
+
+}
+
+//double &f() { return valor; }
